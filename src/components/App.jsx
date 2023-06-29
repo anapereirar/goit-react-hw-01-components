889887d50c94b517a39358ed566d4441868b57c4
Ana <ana.pereirar@utadeo.edu.co>
2023-06-28 23:12:11 -0500

    Tareas completas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { Profile } from './Profile/Profile';
+// import { Statistics} from './Statistics/Statistics'
+import user from '.././data/user.json'
+import stats from '../data/stats.json'
+import Statistics from './Statistics/Statistics';
+
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json'
+
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json'
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
+        gap: '20px'
       }}
     >
-      React homework template
+
+      <Profile user= {user}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+   <Statistics title="Upload stats" stats={stats} /> 
+      <FriendList friends={friends}/>
+       <TransactionHistory items= {transactions}/> 
     </div>
   );
 };
